fc7fac80e6100d5a146967d58d26d3757de358e8
CoDeveloper612 <michaeljoe612@outlook.com>
2025-03-24 10:49:30 -0500

    signup page metadata modified to 'signup'

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -7,7 +7,6 @@ import SignUpForm from "./SignUpForm";
 export const metadata: Metadata = {
   title: "Sign Up",
 };
-
 export default function Page() {
   return (
     <main className="flex h-screen items-center justify-center p-5">
